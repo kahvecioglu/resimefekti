fb7ee2765af39075c75923ba1e8c7456c5717e8b
kahvecioglu <kahvecioglubugra@gmail.com>
2025-03-01 00:17:51 +0300

    added color and edited buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,48 @@
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 import 'package:http/http.dart' as http;
-import 'dart:typed_data';
 import 'package:provider/provider.dart';
 
-// Resim sağlayıcı sınıfı
-class ImageProviderModel with ChangeNotifier {
-  Uint8List? _selectedImage;
-  Uint8List? _processedImage;
-  bool _isLoading = false;
-
-  Uint8List? get selectedImage => _selectedImage;
-  Uint8List? get processedImage => _processedImage;
-  bool get isLoading => _isLoading;
-
-  void updateSelectedImage(Uint8List image) {
-    _selectedImage = image;
-    notifyListeners(); // UI'yi güncelle
-  }
-
-  void updateProcessedImage(Uint8List image) {
-    _processedImage = image;
-    notifyListeners(); // UI'yi güncelle
-  }
-
-  void setLoading(bool isLoading) {
-    _isLoading = isLoading;
-    notifyListeners(); // UI'yi güncelle
-  }
-}
-
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => ImageProviderModel(),
-      child: ImageFilterApp(),
-    ),
-  );
+  runApp(MyApp());
 }
 
-class ImageFilterApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => ImageProviderModel(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
     );
+  }
+}
+
+class ImageProviderModel extends ChangeNotifier {
+  Uint8List? selectedImage;
+  Uint8List? processedImage;
+  bool isLoading = false;
+
+  // Seçilen resmi güncelle
+  void updateSelectedImage(Uint8List? image) {
+    selectedImage = image;
+    notifyListeners();
+  }
+
+  // İşlenmiş resmi güncelle
+  void updateProcessedImage(Uint8List? image) {
+    processedImage = image;
+    notifyListeners();
+  }
+
+  // Yükleniyor durumunu ayarla
+  void setLoading(bool loading) {
+    isLoading = loading;
+    notifyListeners();
   }
 }
 
@@ -84,6 +80,9 @@ class _HomeScreenState extends State<HomeScreen> {
     imageProvider.setLoading(true);
     _errorMessage = '';
 
+    // 5 saniye bekle simülasyonu
+    await Future.delayed(Duration(seconds: 12));
+
     var request = http.MultipartRequest(
       'POST',
       Uri.parse('http://192.168.1.103:5000/process_image'), // API URL
@@ -98,9 +97,7 @@ class _HomeScreenState extends State<HomeScreen> {
     request.fields['filter'] = filterType;
 
     try {
-      var response = await request
-          .send()
-          .timeout(Duration(seconds: 60)); // 60 saniye timeout
+      var response = await request.send();
 
       if (response.statusCode == 200) {
         final bytes = await response.stream.toBytes();
@@ -115,88 +112,224 @@ class _HomeScreenState extends State<HomeScreen> {
         _errorMessage = 'API hatası: ${response.statusCode}';
       }
     } catch (e) {
-      _errorMessage = 'API bağlantı hatası: $e';
+      _errorMessage = 'Bağlantı hatası Tekrar deneyin: $e';
     } finally {
-      imageProvider.setLoading(false);
+      imageProvider.setLoading(false); // Yükleniyor durumunu sonlandır
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 7, 45, 77),
       appBar: AppBar(
-        title: Text('Resim Filtreleme Uygulaması'),
+        backgroundColor: const Color.fromARGB(255, 7, 45, 77),
+        title: Text(
+          'Resim Filtreleme Uygulaması',
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
       ),
       body: Consumer<ImageProviderModel>(
         builder: (context, imageProvider, child) {
-          return SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Seçilen resmi göster
-                imageProvider.selectedImage == null
-                    ? Text('Lütfen bir resim seçin.')
-                    : Image.memory(imageProvider.selectedImage!),
-                SizedBox(height: 20),
+          return Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Seçilen resmi göster
+                  imageProvider.selectedImage == null
+                      ? Text(
+                          'Lütfen bir resim seçin.',
+                          style: TextStyle(color: Colors.white),
+                        )
+                      : Image.memory(imageProvider.selectedImage!),
+                  SizedBox(height: 20),
 
-                // Filtrelenmiş resmi göster
-                imageProvider.processedImage == null
-                    ? Container()
-                    : Image.memory(imageProvider.processedImage!),
-                SizedBox(height: 20),
+                  // Filtrelenmiş resmi göster
+                  imageProvider.processedImage == null
+                      ? Container()
+                      : Image.memory(imageProvider.processedImage!),
+                  SizedBox(height: 20),
 
-                // Resim seçme butonu
-                ElevatedButton(
-                  onPressed: _pickImage,
-                  child: Text('Resim Seç'),
-                ),
-                SizedBox(height: 20),
+                  // Hata mesajı
+                  _errorMessage.isNotEmpty
+                      ? Text(_errorMessage, style: TextStyle(color: Colors.red))
+                      : Container(),
+                ],
+              ),
 
-                // Filtre uygulama butonları
-                ElevatedButton(
-                  onPressed: () => _applyFilter('grayscale'),
-                  child: Text('Grayscale Uygula'),
+              // Yükleniyor göstergesi, resimlerin üstünde
+              if (imageProvider.isLoading)
+                Positioned.fill(
+                  child: Container(
+                    color: Colors.black.withOpacity(
+                        0.5), // Sayfanın geri kalanını karartmak için
+                    child: Center(
+                      child: CircularProgressIndicator(
+                        strokeWidth: 5, // Geniş yükleme çubuğu
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  ),
                 ),
-                ElevatedButton(
-                  onPressed: () => _applyFilter('blur'),
-                  child: Text('Blur Uygula'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _applyFilter('sepia'),
-                  child: Text('Sepia Uygula'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _applyFilter('invert'),
-                  child: Text('Invert Uygula'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _applyFilter('edge'),
-                  child: Text('Edge Uygula'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _applyFilter('contrast'),
-                  child: Text('Contrast Uygula'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _applyFilter('brightness'),
-                  child: Text('Brightness Uygula'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _applyFilter('mirror'),
-                  child: Text('Mirror Uygula'),
-                ),
-                SizedBox(height: 20),
 
-                // Yükleniyor göstergesi veya hata mesajı
-                imageProvider.isLoading
-                    ? CircularProgressIndicator()
-                    : (_errorMessage.isNotEmpty
-                        ? Text(_errorMessage,
-                            style: TextStyle(color: Colors.red))
-                        : Container()),
-              ],
-            ),
+              // Sabit butonlar, ekranın altında
+              Positioned(
+                bottom: 20,
+                left: 0,
+                right: 0,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Resim seçme butonu
+                      ElevatedButton(
+                        onPressed: imageProvider.isLoading ? null : _pickImage,
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color.fromARGB(255, 7, 71, 148)), // Açık Mavi
+                          foregroundColor: MaterialStateProperty.all(
+                              Color(0xFFFFFFFF)), // Beyaz yazı
+                        ),
+                        child: Text('+'),
+                      ),
+                      SizedBox(width: 20),
+                      ElevatedButton(
+                        onPressed: imageProvider.isLoading
+                            ? null
+                            : () => _applyFilter('grayscale'),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color(0xFF00BFFF)), // Açık Mavi
+                          foregroundColor: MaterialStateProperty.all(
+                              Color(0xFFFFFFFF)), // Beyaz yazı
+                        ),
+                        child: Text('Grayscale'),
+                      ),
+                      SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: imageProvider.isLoading
+                            ? null
+                            : () => _applyFilter('blur'),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color(0xFF00BFFF)), // Açık Mavi
+                          foregroundColor: MaterialStateProperty.all(
+                              Color(0xFFFFFFFF)), // Beyaz yazı
+                        ),
+                        child: Text('Blur'),
+                      ),
+                      SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: imageProvider.isLoading
+                            ? null
+                            : () => _applyFilter('sepia'),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color(0xFF00BFFF)), // Açık Mavi
+                          foregroundColor: MaterialStateProperty.all(
+                              Color(0xFFFFFFFF)), // Beyaz yazı
+                        ),
+                        child: Text('Sepia'),
+                      ),
+                      SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: imageProvider.isLoading
+                            ? null
+                            : () => _applyFilter('invert'),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color(0xFF00BFFF)), // Açık Mavi
+                          foregroundColor: MaterialStateProperty.all(
+                              Color(0xFFFFFFFF)), // Beyaz yazı
+                        ),
+                        child: Text('Invert'),
+                      ),
+                      SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: imageProvider.isLoading
+                            ? null
+                            : () => _applyFilter('edge'),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color(0xFF00BFFF)), // Açık Mavi
+                          foregroundColor: MaterialStateProperty.all(
+                              Color(0xFFFFFFFF)), // Beyaz yazı
+                        ),
+                        child: Text('Edge'),
+                      ),
+                      SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: imageProvider.isLoading
+                            ? null
+                            : () => _applyFilter('contrast'),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color(0xFF00BFFF)), // Açık Mavi
+                          foregroundColor: MaterialStateProperty.all(
+                              Color(0xFFFFFFFF)), // Beyaz yazı
+                        ),
+                        child: Text('Contrast'),
+                      ),
+                      SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: imageProvider.isLoading
+                            ? null
+                            : () => _applyFilter('brightness'),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color(0xFF00BFFF)), // Açık Mavi
+                          foregroundColor: MaterialStateProperty.all(
+                              Color(0xFFFFFFFF)), // Beyaz yazı
+                        ),
+                        child: Text('Brightness'),
+                      ),
+                      SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: imageProvider.isLoading
+                            ? null
+                            : () => _applyFilter('mirror'),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color(0xFF00BFFF)), // Açık Mavi
+                          foregroundColor: MaterialStateProperty.all(
+                              Color(0xFFFFFFFF)), // Beyaz yazı
+                        ),
+                        child: Text('Mirror'),
+                      ),
+                      SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: imageProvider.isLoading
+                            ? null
+                            : () => _applyFilter('negative'),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color(0xFF00BFFF)), // Açık Mavi
+                          foregroundColor: MaterialStateProperty.all(
+                              Color(0xFFFFFFFF)), // Beyaz yazı
+                        ),
+                        child: Text('Negative'),
+                      ),
+                      SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: imageProvider.isLoading
+                            ? null
+                            : () => _applyFilter('vignette'),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color(0xFF00BFFF)), // Açık Mavi
+                          foregroundColor: MaterialStateProperty.all(
+                              Color(0xFFFFFFFF)), // Beyaz yazı
+                        ),
+                        child: Text('Vignette'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           );
         },
       ),
--- python/app.py
+++ python/app.py
@@ -7,6 +7,7 @@ import io
 
 app = Flask(__name__)
 CORS(app)  # CORS ayarları
+
 @app.route('/process_image', methods=['POST'])
 def process_image():
     try:
@@ -20,13 +21,16 @@ def process_image():
         img = Image.open(file.stream)
         img = np.array(img)
 
+        # Resmin derinliğini kontrol et ve gerekirse 8-bit'e dönüştür
+        if img.dtype != np.uint8:
+            img = cv2.convertScaleAbs(img)
+
         # Filtre uygula
         if filter_type == 'grayscale':
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         elif filter_type == 'blur':
             img = cv2.GaussianBlur(img, (15, 15), 0)
         elif filter_type == 'sepia':
-            # Sepia filtresi örneği
             kernel = np.array([[0.393, 0.769, 0.189],
                                [0.349, 0.686, 0.168],
                                [0.272, 0.534, 0.131]])
@@ -36,18 +40,31 @@ def process_image():
         elif filter_type == 'edge':
             img = cv2.Canny(img, 100, 200)
         elif filter_type == 'contrast':
-            img = cv2.convertScaleAbs(img, alpha=1.5, beta=0)  # Kontrast artırma
+            img = cv2.convertScaleAbs(img, alpha=1.5, beta=0)
         elif filter_type == 'brightness':
-            img = cv2.convertScaleAbs(img, alpha=1, beta=50)  # Parlaklık artırma
+            img = cv2.convertScaleAbs(img, alpha=1, beta=50)
         elif filter_type == 'mirror':
-            img = cv2.flip(img, 1)  # Ayna efekti
+            img = cv2.flip(img, 1)
+        elif filter_type == 'negative':
+            img = 255 - img
+        elif filter_type == 'vignette':
+            rows, cols = img.shape[:2]
+            X_result = np.zeros((rows, cols, 3), dtype=np.uint8)
+            for i in range(0, rows):
+                for j in range(0, cols):
+                    dist = np.sqrt((i - rows/2) ** 2 + (j - cols/2) ** 2)
+                    factor = 1 - min(dist / (rows / 2), 1)
+                    X_result[i, j] = img[i, j] * factor
+            img = X_result
+        
         else:
             return jsonify({'error': 'Geçersiz filtre türü.'}), 400
 
         # Resmi sıkıştır ve gönder
-        _, img_encoded = cv2.imencode('.jpg', img, [int(cv2.IMWRITE_JPEG_QUALITY), 50])  # JPEG kalitesini düşür
+        _, img_encoded = cv2.imencode('.jpg', img, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
         return img_encoded.tobytes(), 200, {'Content-Type': 'image/jpeg'}
     except Exception as e:
         return jsonify({'error': f'Resim işlenirken bir hata oluştu: {str(e)}'}), 500
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)  # Threaded modunu etkinleştir
+    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
